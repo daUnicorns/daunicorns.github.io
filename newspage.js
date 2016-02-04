7f45a8a644e5e5dd9f1ccbbff4a33639c9b7034c
RobStallion <Robertefrancis18@gmail.com>
2016-02-04 11:37:46 +0000

    updated function names

--- newspage.js
+++ newspage.js
@@ -1,50 +1,40 @@
-var getUnanswered = function(topic) {
+var getArticals = function(topic) {
   var req = new XMLHttpRequest();
   var parsed;
-
   var guardianAPI = "6509f558-5d11-416c-840b-78ccfb4a0837";
   var url = "http://content.guardianapis.com/search?api-key=6509f558-5d11-416c-840b-78ccfb4a0837&q=" + topic + "&show-elements=all&show-fields=headline,thumbnail,body";
+
   req.onreadystatechange = function () {
     if (req.readyState === 4 && req.status === 200)   {
       parsed = JSON.parse(req.response);
-
-
       // var results = parseQuestionData(parsed.response.results);
     }
   };
-
   req.open("GET", url, false);
   req.send();
   var arr = parsed.response.results;
-  appendQuestionResultsToDOM(arr);
+  appendArticlesToDOM(arr);
 };
-
 
 document.getElementById('news-topic').addEventListener('submit',function(e){
   e.preventDefault();
   document.getElementById('results').innerHTML+="";
   var topic = document.querySelector('#news-topic input[type="text"]').value;
-  console.log(topic);
-  var resultsArray = getUnanswered(topic);
+  // console.log(topic);
+  var resultsArray = getArticals(topic);
 });
 
 
-var appendQuestionResultsToDOM = function(results) {
+var appendArticlesToDOM = function(results) {
   results.forEach(function(el) {
     document.write('<article>');
     document.write('<h2>'+ el.webTitle +'</h2>');
-    // document.write('<p>'+ el.content+'</p>');
+    // console.log(el.fields.body)
+    document.write('<p>'+ el.webPublicationDate +'</p>');
     // document.write('<p>'+ el.date+'</p>');
     document.write('</article>');
-    // twitter
   });
 };
-
-
-
-
-
-
 
 // var parseQuestionData = function(articles) {
 //     // var parsedResults = JSON.parse(articles.results);
