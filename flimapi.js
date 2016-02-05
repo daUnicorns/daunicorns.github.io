4ac96d168d674832a648544e3190ee4ac920c5b9
eleanorreem <eleanor.reem@hotmail.co.uk>
2016-02-05 10:09:37 +0000

    fixed the poster link

--- flimapi.js
+++ flimapi.js
@@ -31,9 +31,9 @@ document.getElementById('news-movie').addEventListener('submit',function(e){
     for(var o in results) {
       filmresults.push(results[o]);
     }
-
+    console.log(filmresults);
     document.getElementById("filmresults").innerHTML =
-    "<article id = 'filmarticle'> <div id='poster'><img id = 'leposter' src='http://vignette3.wikia.nocookie.net/adventuretimewithfinnandjake/images/a/a9/Adventure-time-dance-5046_preview.gif/revision/latest?cb=20110617014146'/></div> <div id='filmdetails'> <h1 id='title'>" +
+    "<article id = 'filmarticle'> <div id='poster'><img id = 'leposter' src='" + filmresults[13] + "'/></div> <div id='filmdetails'> <h1 id='title'>" +
     filmresults[0] + "</h1> <h2 id='year'>" +  filmresults[1] + "</h2> <p id='cast'><strong>Actors : </strong>" +
     filmresults[8] + "</p> <p id='rating'><strong>IMDB rating : </strong>"+ filmresults[15] +
      "</p> </div> <div id='plot'> <p id='plotcontent'><strong>Plot : </strong>" + filmresults[9] +
